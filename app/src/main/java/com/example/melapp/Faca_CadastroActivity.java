572e2af7b5865fdf1ef31aa9852740a55b4ae01e
estefany1816 <estefanysilvagaoncalves@gmail.com>
2021-12-09 19:14:11 -0300

    atualização simples das telas de Projeto Integrador - 09/12/2021 - Estefany

---
+++ app/src/main/java/com/example/melapp/Faca_CadastroActivity.java
@@ -0,0 +1,13 @@
+package com.example.melapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Faca_CadastroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.faca_cadastro_activity);
+    }
+}
