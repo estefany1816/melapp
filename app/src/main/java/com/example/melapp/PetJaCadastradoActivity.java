418eb66b2cc734d0bc679ea087311e8631e83fc1
estefany1816 <estefanysilvagaoncalves@gmail.com>
2021-12-17 10:39:44 -0300

    Todas as telas da 1ª etapa foram criadas. Projeto Integrador - 17/12/2021 - Estefany

---
+++ app/src/main/java/com/example/melapp/PetJaCadastradoActivity.java
@@ -0,0 +1,14 @@
+package com.example.melapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PetJaCadastradoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pet_ja_cadastrado);
+    }
+}
---
+++ app/src/main/java/com/example/melapp/TesteAuxiliarActivity.java
@@ -0,0 +1,14 @@
+package com.example.melapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TesteAuxiliarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste_auxiliar);
+    }
+}
